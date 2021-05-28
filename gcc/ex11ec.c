98c98ec592608b9a72d75c6b14baad0f9c967ca3
Adam Szymanowski <adam.szym@yahoo.pl>
2021-05-28 07:33:46 +0200

    Add another example in Exercise 11 Extra Credtit.

--- gcc/ex11ec.c
+++ gcc/ex11ec.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 // Extra Credit
 int main()
 {
@@ -17,7 +18,7 @@ int main()
     printf("reverse-engineering:\n");
     int attempt1 = name[0] << 24 | name[1] << 16 | name[2] << 8 | name[3];
     printf("attempt 1 int value: %d\n", attempt1);
-    char *pointer = &attempt1;
+    char *pointer = (char *)&attempt1;
     printf("at %p value\t%d\t('%c')\n", pointer, *pointer, *pointer);
     printf("at %p value\t%d\t('%c')\n", pointer+1, *(pointer+1), *(pointer+1));
     printf("at %p value\t%d\t('%c')\n", pointer+2, *(pointer+2), *(pointer+2));
@@ -26,12 +27,39 @@ int main()
 
     int attempt2 = name[3] << 24 | name[2] << 16 | name[1] << 8 | name[0];
     printf("attempt 2 int value: %d\n", attempt2);
-    pointer = &attempt2;
+    pointer = (char *)&attempt2;
     printf("at %p value\t%d\t('%c')\n", pointer, *pointer, *pointer);
     printf("at %p value\t%d\t('%c')\n", pointer+1, *(pointer+1), *(pointer+1));
     printf("at %p value\t%d\t('%c')\n", pointer+2, *(pointer+2), *(pointer+2));
     printf("at %p value\t%d\t('%c')\n", pointer+3, *(pointer+3), *(pointer+3));
     printf("\n");
+
+    // antoher extra stuff using code from:
+    // https://en.wikipedia.org/wiki/Endianness#Optimization
+    printf("Extra example:\n");
+    uint64_t value = 0x4A;
+    union {
+        uint8_t u8; uint16_t u16; uint32_t u32; uint64_t u64;
+    } u = { .u64 = value };
+
+    printf("0x%lx (%lu) as:\n", value, value);
+    printf(" 8-bit unsigned integer: %d\n", u.u8);
+    printf("16-bit unsigned integer: %d\n", u.u16);
+    printf("32-bit unsigned integer: %d\n", u.u32);
+    printf("64-bit unsigned integer: %lu\n", u.u64);
+    puts(u.u8 == u.u16 && u.u8 == u.u32 && u.u8 == u.u64 ? "true" : "false");
+
+    printf("\nNow using attempt2 as value:\n");
+
+    value = attempt2;
+    u.u32 = value;
+
+    printf("0x%lx (%lu) as:\n", value, value);
+    printf(" 8-bit unsigned integer: %d\n", u.u8);
+    printf("16-bit unsigned integer: %d\n", u.u16);
+    printf("32-bit unsigned integer: %d\n", u.u32);
+    printf("64-bit unsigned integer: %lu\n", u.u64);
+    puts(u.u8 == u.u16 && u.u8 == u.u32 && u.u8 == u.u64 ? "true" : "false");
 
     return 0;
 }
